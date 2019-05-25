8740b57085b782a609437cc35815cd078b941305
Ihorbar1 <31664855+Ihorbar1@users.noreply.github.com>
2019-05-25 22:01:39 +0300

    test

--- my_theme/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<p>copy <?php echo date("Y")?></p>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-
-</html>
--- my_theme/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if(is_active_sidebar('sidebar-right')): ?>
-	<aside>
-		<?php dynamic_sidebar('sidebar-right') ?>
-	</aside>
-<? endif; ?>
